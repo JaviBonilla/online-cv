12a7ce1f7f9b0ce45c686336a4db910f4fbe3386
Javier Bonilla <javier.bonilla@psa.es>
2023-06-12 08:38:36 +0200

    Updated thumbnail generator script.

--- assets/docs/thumb.sh
+++ assets/docs/thumb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 for f in *.pdf
 do 
-    convert -thumbnail x300 -background white -alpha remove "$f"[0] "${f%.pdf}.png"
+    file_png="${f//.pdf/.png}"
+    if [ ! -f $file_png ]; then
+        echo $file_png ' created.'
+        convert -thumbnail x300 -background white -alpha remove "$f"[0] "${f%.pdf}.png"
+    fi
 done
