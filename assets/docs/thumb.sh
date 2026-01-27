11e2175fdc402505a0bbafd82a96871f54ada070
Javier Bonilla <javier.bonilla@psa.es>
2026-01-27 14:51:58 +0100

    Added CIES 2026 and Tewer collaboration. Updated: "vida laboral", overview metrics and index. New thumb script to convert directly to webp.

--- assets/docs/thumb.sh
+++ assets/docs/thumb.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-for f in *.pdf
-do 
-    file_png="${f//.pdf/.png}"
-    if [ ! -f $file_png ]; then
-        echo $file_png ' created.'
-        convert -thumbnail x300 -background white -alpha remove "$f"[0] "${f%.pdf}.png"
+
+for f in *.pdf; do
+    file_webp="${f%.pdf}.webp"
+
+    if [ ! -f "$file_webp" ]; then
+        echo "$file_webp created."
+        convert \
+            "$f"[0] \
+            -thumbnail x300 \
+            -background white \
+            -alpha remove \
+            -quality 85 \
+            "$file_webp"
     fi
 done
